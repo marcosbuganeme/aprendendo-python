7cfcfad712b557733593cf9de7955d9adf8593c2
Marcos Buganeme <molavosbdeveloper@gmail.com>
2024-05-09 22:00:03 -0300

    feat: pep8 - leason 1 - use camel case for classes

---
+++ 1.introducao/pep8.py
@@ -0,0 +1,19 @@
+"""
+PEP8 - Python Enhancement Proposal
+
+Proposals for the improvement of the Python language
+
+The Zem of Python
+
+import this
+
+print(this)
+
+[1] - Use Camel Case for classes;
+
+class Calculator:
+  pass
+
+class CalculatorScientific:
+  pass
+"""
