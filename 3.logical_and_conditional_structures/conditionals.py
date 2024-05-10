dc748e06d68a06af19d395d13ba8778e8dc3aec6
Marcos Buganeme <marcos.after@gmail.com>
2024-05-10 14:39:56 -0300

    chore: move file the directory

---
+++ 3.logical_and_conditional_structures/conditionals.py
@@ -0,0 +1,14 @@
+"""
+structure of conditionals
+if else elif
+"""
+
+age = 18
+
+if age < 18:
+    print(age)
+    print("You are a minor")
+elif age == 18:
+    print("You are 18 years old")
+else:
+    print("You are an adult")
