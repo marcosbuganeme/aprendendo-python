dfde4bc1d70389c5f6fc8afdf881596412504389
Marcos Buganeme <marcos.after@gmail.com>
2024-05-09 23:45:50 -0300

    feat: lesson 2 - scope of variable

---
+++ 2.variable_and_types_data/scope_of_variable.py
@@ -0,0 +1,21 @@
+"""
+Scope of variable:
+    1. Local variable
+    2. Global variable
+"""
+# 1. Local variable
+variable_local = 42
+if variable_local > 10:
+    novo = variable_local + 10
+    #print(novo)
+
+print(novo)
+
+# 2. Global variable
+number = 42
+#print(number)
+#print(type(number))
+
+number = "Minhas bolas"
+#print(number)
+#print(type(number))
