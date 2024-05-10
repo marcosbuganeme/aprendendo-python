a3765738aa4aec4b90ce0115d75c12b69b52de64
Marcos Buganeme <marcos.after@gmail.com>
2024-05-09 23:36:28 -0300

    feat: lesson 2 - string types

---
+++ 2.variable_and_types_data/string_type.py
@@ -0,0 +1,23 @@
+"""
+Type string is a sequence of characters. It is a data type that is used to store text rather than numbers.
+"""
+
+name = 'Marcos \nBuganeme'
+print(name)
+print(type(name))
+
+name = "Can'abis for sales"
+print(name)
+print(type(name))
+
+name = 'Marcos Buganeme'
+print(name)
+print(type(name))
+
+print(name.upper())
+print(name.lower())
+print(name.split())
+print(name.capitalize())
+print(name.__len__())
+print(name.swapcase())
+print(name[::-1])
