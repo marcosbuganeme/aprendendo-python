ab5348d73a5aa4d1f391d762e40dccbf37ceab99
Marcos Buganeme <marcos.after@gmail.com>
2024-05-10 14:42:45 -0300

    feat: logical operator and

---
+++ 3.logical_and_conditional_structures/logical_structure.py
@@ -0,0 +1,19 @@
+"""
+Structure of the logical operators [and, or, not and is]
+
+Operators are used to perform operations on variables and values.
+
+Operator unary: not, is
+
+Operator binary: and, or
+"""
+
+active = True
+logged = True
+
+if active and logged:
+    print("Welcome to the system")
+else:
+    print("You are not allowed to enter the system")
+
+
