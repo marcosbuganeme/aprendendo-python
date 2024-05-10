bd14cd2d45793ae595daa09bd74c937a070d0886
Marcos Buganeme <molavosbdeveloper@gmail.com>
2024-05-09 22:30:20 -0300

    feat: lesson - dir and help

---
+++ 1.introducao/dir_and_help.py
@@ -0,0 +1,13 @@
+"""
+Utilities Python with auxiliary functions and classes
+
+dir -> Returns a list of attributes and methods of a module or object
+
+dir(type data or variable)
+dir("Marcos")
+
+help -> Returns the documentation of the attributes and methods of a module or object
+help("Marcos".upper())
+
+"""
+print(dir("Marcos"))
