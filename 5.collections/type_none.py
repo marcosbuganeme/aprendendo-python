85c3528d3bfcfee62413251520e81878f28b3a33
Marcos Buganeme <marcos.after@gmail.com>
2024-05-12 18:35:01 -0300

    feat: lesson 5 - type none definitions

---
+++ 5.collections/type_none.py
@@ -0,0 +1,13 @@
+"""
+Type None is a special type in Python that represents the absence of a value or a null value.
+
+Type None only has one value, which is None.
+
+Correct -> None
+Invalid -> none, NONE, NonE, nOne, NoNe, noNe, nOnE, nONE
+
+Who is using None?
+- Variables that have not been assigned a value
+- Functions that do not return a value
+- Functions that return a value, but the value is not used
+"""
