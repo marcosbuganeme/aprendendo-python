18fc1e6246835b0ddd8fbdefaabbc1c34ec33475
Marcos Buganeme <marcos.after@gmail.com>
2024-05-12 07:06:03 -0300

    feat: lesson 5 - list - checking if a number exists within the list

---
+++ 5.collections/list.py
@@ -0,0 +1,16 @@
+"""
+List is a collection which is ordered and changeable.
+Allows duplicate members.
+List in python have dynamic size, so it can grow and shrink as per the requirement.
+List in python are mutable, so they can be modified after creation.
+"""
+
+type([]) # <class 'list'>
+
+listOfNumbers = [1, 2, 3, 3, 3, 3, 3, 3, 3, 14, 15]
+if 15 in listOfNumbers:
+    print("Number 15 is in the list")
+else:
+    print("Number 15 is not in the list")
+print(f'Number is 3 = {listOfNumbers.count(3)} x')
+
