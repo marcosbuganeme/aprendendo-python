210cfa35bf881442c6f23edcf20f58c626df207b
Marcos Buganeme <marcos.after@gmail.com>
2024-05-12 18:23:07 -0300

    feat: lesson 5 - definition at dictionary

---
+++ 5.collections/dictionary.py
@@ -0,0 +1,9 @@
+"""
+Dictionary in python is an unordered collection of data in a key:value pair form.
+
+Dictionary is a collection which is unordered, changeable and indexed. In Python dictionaries are written with curly brackets, and they have keys and values.
+
+Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+
+Keys are unique within a dictionary while values may not be. The values of a dictionary can be of any type, but the keys must be of an immutable data type such as strings, numbers, or tuples.
+"""
