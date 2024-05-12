deb9a915d30705065197d6d246c88f71cdcdfb62
Marcos Buganeme <marcos.after@gmail.com>
2024-05-12 18:26:23 -0300

    feat: lesson 5 - create a dictionary example 1

--- 5.collections/dictionary.py
+++ 5.collections/dictionary.py
@@ -6,4 +6,29 @@ Dictionary is a collection which is unordered, changeable and indexed. In Python
 Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
 
 Keys are unique within a dictionary while values may not be. The values of a dictionary can be of any type, but the keys must be of an immutable data type such as strings, numbers, or tuples.
+
+print(type({}))
+
 """
+
+print(type({}))
+
+# Creating a dictionary - Example 1
+
+countries = {
+    'BR': 'Brazil',
+    'US': 'United States',
+    'JP': 'Japan',
+    'DE': 'Germany',
+    'IT': 'Italy',
+    'FR': 'France',
+    'UK': 'United Kingdom',
+    'CN': 'China',
+    'IN': 'India',
+    'RU': 'Russia'
+}
+
+print(countries)
+
+print(type(countries))
+
