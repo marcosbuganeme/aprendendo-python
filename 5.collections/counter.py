94dccd3c554d708ce844e2712a72f22e96e618b2
Marcos Buganeme <marcos.after@gmail.com>
2024-05-13 17:25:39 -0300

    feat: lesson 5 - import Counter

---
+++ 5.collections/counter.py
@@ -0,0 +1,14 @@
+"""
+Module Collections - Counter
+
+Counter -> High-performance container datatypes
+
+Counter is a subclass of dictionary class. It is used to keep the count of the elements in an iterable in the form of an unordered collection of elements. It is an unordered collection where elements are stored as dictionary keys and their counts are stored as dictionary values.
+
+Collections in Python are containers that are used to store collections of data, for example, list, dict, set, tuple etc. These are built-in collections. Several modules have been developed that provide additional data structures to store collections of data. One such module is the Python Counter module.
+"""
+
+# Use of Counter
+
+from collections import Counter
+
