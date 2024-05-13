76682d24324157b39e96168292a71a8005142397
Marcos Buganeme <marcos.after@gmail.com>
2024-05-13 18:07:22 -0300

    feat: lesson 5 - review tuple

---
+++ 5.collections/named_tuple.py
@@ -0,0 +1,13 @@
+"""
+Module Collections - Named Tuple
+
+Named tuple is a tuple with named fields. It is a subclass of the tuple class.
+"""
+
+# Review Tuple
+
+my_tuple = (1, 2, 3)
+print(my_tuple[0])
+
+
+
