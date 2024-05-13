b6f3a5981367f1153e604ef1b92d2d6dc4f6214f
Marcos Buganeme <marcos.after@gmail.com>
2024-05-13 18:03:16 -0300

    feat: lesson 5 - order dict

--- 5.collections/ordered_dict.py
+++ 5.collections/ordered_dict.py
@@ -8,3 +8,34 @@ print(dictionary)
 # The order of the elements is not guaranteed
 for key, value in dictionary.items():
     print(f'key={key}, value={value}')
+
+
+from collections import OrderedDict
+
+# Ordered Dict maintains the order of the elements
+# Ensure the order of insertion of elements
+
+ordered_dictionary = OrderedDict({'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5})
+print(ordered_dictionary)
+
+for key, value in ordered_dictionary.items():
+    print(f'key={key}, value={value}')
+
+
+# Difference between Dict and Ordered Dict
+
+# Dict
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 2, 'a': 1}
+
+# Order the elements in the dictionary does not matter
+print(dict1 == dict2) # True
+
+
+# Ordered Dict
+
+dict1 = OrderedDict({'a': 1, 'b': 2})
+dict2 = OrderedDict({'b': 2, 'a': 1})
+
+print(dict1 == dict2) # False
