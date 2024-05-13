826eb183de024a7ac64109c0ec77b7195fb00259
Marcos Buganeme <marcos.after@gmail.com>
2024-05-13 18:20:04 -0300

    feat: lesson 5 - create a deque int

---
+++ 5.collections/deque.py
@@ -0,0 +1,15 @@
+"""
+Module Collections - Deque
+
+We can use the deque class to create a double-ended queue. It is a subclass of the list.
+
+High-performance container datatypes
+"""
+
+from collections import deque
+
+# Create a deque int
+
+my_deque = deque([1, 2, 3, 4, 5])
+print(my_deque)
+
