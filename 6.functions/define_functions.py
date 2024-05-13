136e7e54a61890f3cfcb8a77fe553f0c341d39df
Marcos Buganeme <marcos.after@gmail.com>
2024-05-13 18:24:13 -0300

    feat: lesson 6 - definition of functions

---
+++ 6.functions/define_functions.py
@@ -0,0 +1,8 @@
+"""
+Define Functions
+
+1. Functions are a block of code that performs a specific task. 2. Functions are reusable code blocks that can be called multiple times in a program.
+3. Functions are defined using the def keyword followed by the function name and parentheses.
+4. The function body is indented.
+"""
+
